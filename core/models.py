0fc5e86638d62991f8f92051f59d9e265d530ada
FERRNAND0 <luxovgg@gmail.com>
2026-05-14 03:35:52 +0500

    piggybank fix 3

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 import random
 from django.utils import timezone
 from datetime import timedelta
